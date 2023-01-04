a352c1f425b44d2e78bf467b5a9dec4b50e15e14
umoh christian <umohchristian001@gmail.com>
2023-01-04 13:45:57 -0800

    12. fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def fizzbuzz():
+    chr = []
     for i in range(1, 101):
-    if i == 100:
-        print("Buzz")
-    else:
-        if i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print(i, end=" ")
+        chr.append(i)
+
+    for i in range(0, len(chr)):
+        if chr[i] % 3 == 0 and chr[i] % 5 == 0:
+            chr[i] = "FizzBuzz"
+        elif chr[i] % 3 == 0:
+            chr[i] = "Fizz"
+        elif chr[i] % 5 == 0:
+            chr[i] = "Buzz"
+
+    for i in chr:
+        print(i, end=" ")
